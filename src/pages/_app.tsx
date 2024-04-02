058d3bda045802058ff0a5f9dd4b65f423f15af4
Danu Oprea <85162660+danop-dev@users.noreply.github.com>
2024-04-02 16:05:20 +0300

    :heavy_plus_sign: Install shadcn-ui :lipstick: Add theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+const roboto = Montserrat({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className={roboto.className}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
