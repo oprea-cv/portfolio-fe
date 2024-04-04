2a64595d3ba8d38f5ae8826eee39fd58cc70bd18
Danu Oprea <danop-dev@users.noreply.github.com>
2024-04-04 22:26:30 +0300

    :lipstick: :heavy_plus_sign: Add ui components from lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,7 @@ const roboto = Montserrat({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <div className={roboto.className}>
         <Component {...pageProps} />
       </div>
