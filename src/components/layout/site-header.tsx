4a53c5dbfb6f74f210168140c5182fcb0a1cd126
Danu Oprea <85162660+danop-dev@users.noreply.github.com>
2024-04-10 17:00:01 +0300

    :adhesive_bandage: Fix header absolute

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -35,7 +35,7 @@ export function SiteHeader() {
   return (
     <motion.header
       className={cn(
-        "sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-between",
+        "absolute top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-between",
         "transition-all duration-300 ease-in-out",
         "flex justify-center items-center",
       )}
